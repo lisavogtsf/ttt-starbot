f4436ddc24e968bcb257ecc6bd3731ec820cd3dc
Lisa Vogt <lisavogt.sf@gmail.com>
2016-05-29 20:29:28 -0700

    adds placeholder files for tic tac toe game itself based on slack-connect-4-bot

---
+++ src/tic-tac-toe/boardStringGenerator.js
@@ -0,0 +1,1 @@
+boardStringGenerator.js
---
+++ src/tic-tac-toe/square.js
@@ -0,0 +1,27 @@
+// from slack-connect-4-bot
+
+const ColourEnum = require('./colourEnum');
+const Emoji = require('./emoji');
+
+class Slot {
+
+    constructor(row, col, gameType) {
+        this.row = row;
+        this.col = col;
+        this.gameType = gameType;
+        this.empty = true;
+        this.symbol = Emoji[this.gameType].circle.white;
+    }
+
+    play(colour) {
+        this.empty = false;
+        this.colour = colour;
+        this.symbol = Emoji[this.gameType].circle[this.colour];
+    }
+
+    toString() {
+        return this.symbol;
+    }
+}
+
+module.exports = Slot;
